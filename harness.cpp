19eaf5bc048f31fc8cd0d4246f8f7255b8fb0930
devtty1er <devtty1er@gmail.com>
2022-08-25 09:38:32 -0400

    wtf

--- harness.cpp
+++ harness.cpp
@@ -10,21 +10,18 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     return;
   }
   qr = quirc_new();
-  int num_codes, i;
+  int i;
   int w = 100;
   int h = (size - (size % w)) / w;
   uint8_t *buff = quirc_begin(qr, &w, &h);
   memcpy(buff,data,w*h);
   quirc_end(qr);
-  num_codes = quirc_count(qr);
+  int num_codes = quirc_count(qr);
   for (i = 0; i < num_codes; i++) {
     struct quirc_code code;
     struct quirc_data data;
     quirc_decode_error_t err;
-
     quirc_extract(qr, i, &code);
-
-    /* Decoding stage */
     err = quirc_decode(&code, &data);
     }
 }
